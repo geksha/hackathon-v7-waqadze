5471ccb4a88a5a3a6d36132a01269aa3050588be
geksha <ccsgo277@gmail.com>
2024-09-29 17:50:33 +0400

    commit

---
+++ py-hard-4.py
@@ -0,0 +1,3 @@
+print ("""hello! today i will tell you about best academy ever! Goal_oriented_academ! this name describes itself very well.
+             in goa we ar oriented to one goal beeng succesful.in goa games and stuf that can get in your way of beeng succesful are not allowed. 
+            evry one is wellcome here no matter what.join us and be part of our family """)
